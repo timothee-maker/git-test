3312439dac1e8119e35ea1dfa38db80c80bbcee2
Timothée Nolent <tnolent29@gmail.com>
2024-09-08 09:15:57 +0200

    okay

--- piscine/ft_is_prime.c
+++ piscine/ft_is_prime.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int ft_is_prime(int nb)
 {
-    if ft_is_prime 
+    if ((nb % ft_is_prime(nb - 1)) == 0)
+        return 0;
+    else
+        return 1;
+}
+int main(int argc, char **argv)
+{
+    if (argc == 1)
+        printf("%d", ft_is_prime(atoi(argv[1])));
+    return 0;
 }
