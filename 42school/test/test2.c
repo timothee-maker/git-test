504a65224055e583023bc64576803f526a173a7b
Timothee Nolent <tnolent@bess-f4r5s9.clusters.42paris.fr>
2024-12-16 09:57:56 +0100

    mise a jour du repo perso, libft, gnl et printf pushed

--- 42school/test/test2.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int	main()
-{
-	printf("%s", "Apaganin");
-	return (0);
-}
--- piscine/piscine/libft/libft_creator.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-SRC="ft_putchar.c ft_swap.c ft_putstr.c ft_strlen.c ft_strcmp.c"
-gcc -Wall -Wextra -Werror -c $SRC
-ar rc libft.a ft_putchar.o ft_swap.o ft_putstr.o ft_strlen.o ft_strcmp.o
-ranlib libft.a
-rm -f *.o
-
-
